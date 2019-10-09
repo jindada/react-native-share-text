1c5534bb958a9a05e9a079b2145d9f0736e44aaa
jindada <jzs0914@126.com>
2019-10-09 19:54:56 +0800

    init: repository init

---
+++ android/src/main/java/com/jindada/RNWhatsAppShare/RNWhatsAppShareModule.java
@@ -0,0 +1,76 @@
+package com.jindada.RNWhatsAppShare;
+
+import android.content.Intent;
+import android.app.Activity;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RNWhatsAppShareModule extends ReactContextBaseJavaModule {
+
+  private static final int SHARE_QUEST_CODE = 1;
+
+  private final ReactApplicationContext reactContext;
+
+  private static final String ERROR_USER_CANCELLED = "USER_CANCELLED";
+
+  private Promise promise;
+
+  private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+      if (promise != null) {
+        if (resultCode == RESULT_OK) {
+          if (requestCode == SHARE_QUEST_CODE) {
+            promise.resolve();
+          }
+        } else {
+          if (requestCode == SHARE_QUEST_CODE){
+            promise.reject(ERROR_USER_CANCELLED, "User cancelled");
+          }
+        }
+      }
+      promise = null;
+    }
+  };
+
+  public RNWhatsAppShareModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+
+    reactContext.addActivityEventListener(mActivityEventListener);
+    reactContext.addLifecycleEventListener(this);
+  }
+
+  @Override
+  public String getName() {
+    return "RNWhatsAppShare";
+  }
+
+  @ReactMethod
+  public void share(ReadableMap customFields, Promise promise) {
+    this.promise = promise;
+
+    String title = customFields.getString("title");
+    String url = customFields.getString("url");
+
+    Intent intent = new Intent();
+    intent.setAction(Intent.ACTION_SEND);
+    intent.putExtra(Intent.EXTRA_TEXT, "this is my text to send.");
+    intent.setType("text/plain");
+    intent.setPackage("com.whatsapp");
+    startActivityForResult(intent, SHARE_QUEST_CODE);
+  }
+}
